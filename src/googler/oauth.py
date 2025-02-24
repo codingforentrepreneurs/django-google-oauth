b245fe709ef6bc2a26539ebdb7ef13fa557210c5
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-02-24 14:47:20 -0700

    7 - Genereate the Google OAuth Login Url

--- src/googler/oauth.py
+++ src/googler/oauth.py
@@ -1,5 +1,7 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlencode
 from django.conf import settings
+
+from . import security
 
 def get_google_oauth_callback_url(drop_https=False, force_https=False):
     url =  urljoin(settings.BASE_URL, settings.GOOGLE_AUTH_CALLBACK_PATH)
@@ -10,3 +12,33 @@ def get_google_oauth_callback_url(drop_https=False, force_https=False):
     return url
 
 
+def generate_auth_url():
+    redirect_uri = get_google_oauth_callback_url()
+    # public state item
+    state = security.generate_state()
+
+    # private, public
+    code_verifier, code_challenge = security.generate_pkce_pair()
+
+    # google cloud auth platform client id
+    google_auth_client_id = None
+
+    scope = " ".join([
+        "openid",
+        "email", 
+        "profile"
+    ])
+
+    auth_params = {
+        "client_id": google_auth_client_id,
+        "redirect_uri": redirect_uri, 
+        "response_type": "code",
+        "scope": scope,
+        "state": state,
+        "code_challenge": code_challenge,
+        "code_challenge_method": "S256",
+        "access_type": "offline",
+    }
+    encoded_params = urlencode(auth_params)
+    google_oauth_url = "https://accounts.google.com/o/oauth2/v2/auth"
+    return urljoin(google_oauth_url, f"?{encoded_params}")
--- src/googler/views.py
+++ src/googler/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
-# from django.shortcuts import redirect
+from django.shortcuts import redirect
+
+from . import oauth
+# 
 
 def google_login_redirect_view(request):
-    print(request.GET)
-    return HttpResponse("https://google.com redirect")
+    google_oauth2_url = oauth.generate_auth_url()
+    return redirect(google_oauth2_url)
 
 
 def google_login_callback_view(request):
